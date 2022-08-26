7338dae391d7cd6751b2f4e88a018ce8c3e10460
Amine Reda Saf <redasm39@gmail.com>
2022-08-26 02:12:46 +0100

    m00/ex00 : SETUP Zombie.hpp file

--- M01/ex00/Zombie.hpp
+++ M01/ex00/Zombie.hpp
@@ -6,7 +6,23 @@
 /*   By: rsaf <rsaf@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 01:45:33 by rsaf              #+#    #+#             */
-/*   Updated: 2022/08/26 01:45:34 by rsaf             ###   ########.fr       */
+/*   Updated: 2022/08/26 02:06:41 by rsaf             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie
+{
+private:
+    std::string name;
+public:
+    Zombie(/* args */);
+    ~Zombie();
+    void announce( void );
+};
+
+#endif
