a0ba7e7e208dae274221193aee4bcf354e889526
Reda amine Saf <rsaf@e1r2p7.1337.ma>
2022-10-13 21:42:17 +0100

    done

--- M01/ex05/Harl.cpp
+++ M01/ex05/Harl.cpp
@@ -6,7 +6,45 @@
 /*   By: rsaf <rsaf@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 11:43:28 by rsaf              #+#    #+#             */
-/*   Updated: 2022/10/13 11:43:29 by rsaf             ###   ########.fr       */
+/*   Updated: 2022/10/13 21:12:54 by rsaf             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+void Harl::debug(void)
+{
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << "\n";
+}
+
+void Harl::info(void)
+{
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put
+                    enough bacon in my burger! If you did, I wouldn’t be asking for more!" << "\n";
+}
+
+void Harl::warning(void)
+{
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for
+                    years whereas you started working here since last month." << "\n";   
+}
+
+void Harl::error(void)
+{
+    std::cout << "This is unacceptable! I want to speak to the manager now." << "\n";    
+}
+
+void Harl::complain( std::string level)
+{
+    std::cout << "" << "\n";
+}
+
+Harl::Harl()
+{
+    
+}
+
+Harl::~Harl()
+{
+    
+}
