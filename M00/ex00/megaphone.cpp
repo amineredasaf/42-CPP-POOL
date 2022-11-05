a951ba4ac7ded2e11e6b491f3ef1ca31ee11676d
Reda amine Saf <rsaf@e1r8p7.1337.ma>
2022-11-05 22:52:14 +0100

    cpp manner

--- M00/ex00/megaphone.cpp
+++ M00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: rsaf <rsaf@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 14:56:31 by rsaf              #+#    #+#             */
-/*   Updated: 2022/10/31 21:28:15 by rsaf             ###   ########.fr       */
+/*   Updated: 2022/11/05 10:16:28 by rsaf             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 
 int main(int ac, char **av)
 {
-	int     i; 
+	int     i;
 	char    c;
+	std::string line;
 
 	if (ac > 1){
 		for (int x = 1; x < ac; x++){
 			i = -1;
-			while (av[x][++i]){
-				c = toupper(av[x][i]);
+			line = av[x];
+			if (x > 1)
+				std::cout << " ";
+			while (line[++i]){
+				c = toupper(line[i]);
 				std::cout << c;
 			}
 		}
