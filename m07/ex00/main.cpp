f20dbe4a7792700efdfd1936687f215bafb7a05c
Reda amine Saf <redasm39@gmail.com>
2023-01-10 23:51:38 +0100

    -

---
+++ m07/ex00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsaf <rsaf@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/10 19:52:30 by rsaf              #+#    #+#             */
+/*   Updated: 2023/01/10 23:44:53 by rsaf             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main( void ) {
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
+}
---
+++ m07/ex00/whatever.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsaf <rsaf@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/10 19:53:32 by rsaf              #+#    #+#             */
+/*   Updated: 2023/01/10 23:43:58 by rsaf             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+
+template<typename  T>
+
+
+void    swap (T &rhs, T &lhs){
+	
+	T tmp;
+	tmp = rhs;
+	rhs = lhs;
+	lhs = tmp;
+}
+
+template<typename T>
+T	min (T &rhs, T& lhs)
+{
+	if (rhs < lhs)
+		return rhs;
+	return lhs;
+}
+
+template<typename T>
+T	max (T &rhs, T& lhs)
+{
+	if (rhs > lhs)
+		return rhs;
+	return lhs;
+}
